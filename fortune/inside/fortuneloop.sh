84992e9833060d4283b86f98b8b686ec7914579c
chhanz <han0495@gmail.com>
2019-04-08 18:00:08 +0900

    fix script error

--- fortune/inside/fortuneloop.sh
+++ fortune/inside/fortuneloop.sh
@@ -3,7 +3,7 @@ trap "exit" SIGINT
 mkdir /var/htdocs
 while :
 do
-	echo $(date) Writing fortune ro /var/htdocs/index.html
-	/usr/game/fortune > /var/htdocs/index.html
+	echo $(date) Writing fortune to /var/htdocs/index.html
+	/usr/games/fortune > /var/htdocs/index.html
 	sleep 10
 done
